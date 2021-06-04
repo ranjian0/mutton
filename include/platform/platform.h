3602fbe61a0b83791b730aec07b5e1b8b61fb61d
ranjian0 <karanjaichungwa@gmail.com>
2021-06-04 18:32:13 +0300

    prevent input collision

--- include/platform/platform.h
+++ include/platform/platform.h
@@ -3,12 +3,12 @@
 
 #include "utils/Defs.h"
 
+#include "input_desktop.h"
+
+// if we are on android, similar inputs are redefined
+// doing allows building multiplatform apps in one codebase
 #if defined(PLATFORM_ANDROID)
-#   include "input_android.h"
-#elif defined(PLATFORM_WINDOWS) || defined(PLATFORM_APPLE) || defined(PLATFORM_LINUX)
-#   include "input_desktop.h"
-#else 
-#error Unsupported Platform
+#include "input_android.h"
 #endif
 
 
